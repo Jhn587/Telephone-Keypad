6df588142fd9358fbc080761c2c63b499ba7bead
jhn587 <jhn587@gmail.com>
2014-03-28 10:27:07 -0400

    Did work on telephonekeypad

--- TelephoneKeypad.java
+++ TelephoneKeypad.java
@@ -20,6 +20,8 @@ public class TelephoneKeypad {
 
 	private LinkedList<TelephoneData> theList;
 	private String original;
+	
+	//This classes constructor
 	public TelephoneKeypad(String input){
 		theList = new LinkedList<TelephoneData>();
 		populateTheList(this.original);
@@ -29,8 +31,23 @@ public class TelephoneKeypad {
 		
 		int occurances = 0;
 		int numberPressed = 0;
+		
+		//This loop should go through and get each digit and the number of times it was pressed and store it in a linked list
 		for(int i = 0; i<toDecode.length(); i++){
-			if(
+			//If the char we are at is a space we have gone on to the next letter so store the old one
+			//Or if we are at the end of the string
+			if(toDecode.charAt(i) == ' '|| (i == toDecode.length()-1)){
+				//if we are at the last character we need to make sure it matches and increase the number of occurances
+				if(i == toDecode.length()-1 && toDecode.charAt(i)==Character.parseChar(numberPressed)){
+					occurances++;
+				}
+				TelephoneData toStore = new TelephoneData(occurances, numberPressed);
+				this.theList.add(toStore);
+				occurances = 0;//reset counter for occurances of the particular number
+			} else{
+				occurances++;
+				numberPressed = Integer.parseInt(toDecode.charAt(i));
+			}
 		}
 	}
 }
